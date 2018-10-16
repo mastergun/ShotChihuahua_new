575c8bf25b947234904172e1bcc52dd66f8b694d
mastergun <martimendrugu@gmail.com>
2018-10-16 20:02:09 +0200

    adding last things of art

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour {
     enum STATE
@@ -37,11 +38,13 @@ public class AudioManager : MonoBehaviour {
         {
             //musicButton.s
             musicSource.volume = 0;
+            musicButton.GetComponent<Button>().image.sprite = buttonSpriteEffects[(int)STATE.DEACTIVATED];
             musicActivated = false;
         }
         else
         {
             musicSource.volume = 1;
+            musicButton.GetComponent<Button>().image.sprite = buttonSpriteEffects[(int)STATE.ACTIVATED];
             musicActivated = true;
         }
     }
@@ -51,11 +54,13 @@ public class AudioManager : MonoBehaviour {
         if (effectsActivated)
         {
             effectsSource.volume = 0;
+            effectsButton.GetComponent<Button>().image.sprite = buttonSpriteEffects[(int)STATE.DEACTIVATED];
             effectsActivated = false;
         }
         else
         {
             effectsSource.volume = 1;
+            effectsButton.GetComponent<Button>().image.sprite = buttonSpriteEffects[(int)STATE.ACTIVATED];
             effectsActivated = true;
         }
     }
