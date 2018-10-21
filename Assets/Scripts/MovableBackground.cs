3cbfce08f33f3c3cab448330aaf0011a9270b2bc
mastergun <martimendrugu@gmail.com>
2018-10-22 00:45:04 +0200

    fixing upper sky texture

--- Assets/Scripts/MovableBackground.cs
+++ Assets/Scripts/MovableBackground.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovableBackground : MonoBehaviour {
+    Vector2 offset = Vector2.zero;
+    public float speed = 2f;
     public Vector3 initPos;
     public GameObject FlyingObjectRef;
 	// Use this for initialization
@@ -12,6 +14,10 @@ public class MovableBackground : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        offset.x = (offset.x - speed * Time.deltaTime) % 1;
+        if (offset.x < -0.5f) offset.x += 0.5f;
+        this.GetComponent<SpriteRenderer>().material.mainTextureOffset = offset;
+        
         Vector3 newPos = Vector3.zero;
         if (FlyingObjectRef.transform.position.y > initPos.y) newPos.y = FlyingObjectRef.transform.position.y;
         else newPos.y = initPos.y;
